1f90f370974e651c7c66339f27d361054c818e0b
hegdes <hegdes>
2008-07-02 16:04:32 +0000

    excel and xml downloads for cdecart
    SVN-Revision: 216

--- conf/template.load_options.sql
+++ conf/template.load_options.sql
@@ -27,7 +27,9 @@ whenever sqlerror exit sql.sqlcode rollback;
 
 MERGE INTO SBREXT.TOOL_OPTIONS_VIEW_EXT S
 USING (SELECT 'FormBuilder' AS TOOL_NAME, 'URL' AS PROPERTY, 'https://formbuilder@TIER@.nci.nih.gov:6443' AS VALUE, 'The URL for the Form Builder Tool connected this caDSR database.' AS DESCRIPTION, 'US' AS LOCALE FROM DUAL
-UNION SELECT 'FormBuilder' AS TOOL_NAME, 'XML_DOWNLOAD_DIR' AS PROPERTY, '/local/content/formbuilder/output' AS VALUE, 'Download directory for the Form Builder Tool.' AS DESCRIPTION, 'US' AS LOCALE FROM DUAL
+UNION SELECT 'FormBuilder' AS TOOL_NAME, 'XML_DOWNLOAD_DIR' AS PROPERTY, 'D://local/content/formbuilder/output' AS VALUE, 'Download directory for the Form Builder Tool.' AS DESCRIPTION, 'US' AS LOCALE FROM DUAL
+UNION SELECT 'FormBuilder' AS TOOL_NAME, 'XML_PAGINATION_FLAG' AS PROPERTY, 'no' AS VALUE, 'A flag for pagination for download' AS DESCRIPTION, 'US' AS LOCALE FROM DUAL
+UNION SELECT 'FormBuilder' AS TOOL_NAME, 'XML_FILE_MAX_RECORDS' AS PROPERTY, '500' AS VALUE, 'Value for maximum file records' AS DESCRIPTION, 'US' AS LOCALE FROM DUAL
 ) T
 ON (S.TOOL_NAME = T.TOOL_NAME AND S.PROPERTY = T.PROPERTY)
 WHEN MATCHED THEN UPDATE SET S.VALUE = S.VALUE, S.DESCRIPTION = T.DESCRIPTION, S.LOCALE = T.LOCALE
