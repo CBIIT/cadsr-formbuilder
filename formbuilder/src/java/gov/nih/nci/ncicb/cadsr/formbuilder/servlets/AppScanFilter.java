851ffdddd62f11636cf0cf2d9792bb6364044300
mathura <mathura>
2009-06-03 19:25:51 +0000

    added the AppScanFilter servlet for app scan fixes
    SVN-Revision: 456

---
+++ formbuilder/src/java/gov/nih/nci/ncicb/cadsr/formbuilder/servlets/AppScanFilter.java
@@ -0,0 +1,69 @@
+package gov.nih.nci.ncicb.cadsr.formbuilder.servlets;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class AppScanFilter implements Filter {
+
+	private static char[] escapeChars = {'|','&',';','$','%','@','\'','"','\'','\\','+',0x0d,0x0a,',', '<', '>', '(', ')'};
+		
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res,
+			FilterChain filterChain) throws IOException, ServletException {
+		Map paramsMap = req.getParameterMap();
+		Iterator valuesIter = paramsMap.values().iterator();
+		boolean invalid = false;
+		while (valuesIter.hasNext()) {
+			String[] values = (String[])valuesIter.next();
+			for (String value: values) {
+				if (isInvalid(value)) {
+					invalid = true;
+					break;
+				}
+			}
+		}
+		
+		if (invalid) {
+			ServletOutputStream out = res.getOutputStream();
+			out.println("One of the values supplied in the request contains un-permissible characters.");
+			out.println();
+			out.println("Please use the browser back button to go back to the requesting page");
+		}
+		else {
+			filterChain.doFilter(req, res);
+		}
+	}
+	
+	private boolean isInvalid(String paramValue) {
+		boolean invalid = false;
+		
+		if (paramValue != null && paramValue.length() > 0) {
+			for (char escapeChar: escapeChars) {
+				if (paramValue.indexOf(escapeChar) != -1) {
+					invalid = true;
+					break;
+				}
+			}
+		}
+		
+		return invalid;
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+}
