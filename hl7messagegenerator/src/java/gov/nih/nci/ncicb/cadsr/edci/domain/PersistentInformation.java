028a7371e787273fe31282929a44563a53313cc0
no_author <no_author@no_author.cvs2svn>
2009-06-22 21:46:16 +0000

    This commit was manufactured by cvs2svn to create tag 'auto-deploy_build_15061_'.
    SVN-Revision: 492

--- hl7messagegenerator/src/java/gov/nih/nci/ncicb/cadsr/edci/domain/PersistentInformation.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.ncicb.cadsr.edci.domain;
-
-import java.io.Serializable;
-
-public interface PersistentInformation extends Serializable {
-    public void setAssociatedElement(String associatedElement);
-
-    public String getAssociatedElement();
-}
