60961e3fb6e112419ee38d6df1f23774fd18a6d8
no_author <no_author@no_author.cvs2svn>
2009-06-26 18:53:36 +0000

    This commit was manufactured by cvs2svn to create tag 'auto-deploy_build_15127_'.
    SVN-Revision: 504

--- hl7messagegenerator/src/java/gov/nih/nci/ncicb/cadsr/edci/domain/GraphicImage.java
+++
@@ -1,8 +0,0 @@
-package gov.nih.nci.ncicb.cadsr.edci.domain;
-
-public interface GraphicImage extends PersistentInformation{
-
-    public void setSource(String source) ;
-
-    public String getSource() ;
-}
