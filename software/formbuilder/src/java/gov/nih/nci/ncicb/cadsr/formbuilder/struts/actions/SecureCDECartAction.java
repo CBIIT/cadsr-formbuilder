01a9527c24d4d20bd8b4a88eaf62781bc758db1a
avdicd <avdicd>
2010-04-30 14:32:00 +0000

    Updates for multiple cart support.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@546 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/formbuilder/src/java/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/SecureCDECartAction.java
+++ software/formbuilder/src/java/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/SecureCDECartAction.java
@@ -17,11 +17,13 @@ import gov.nih.nci.ncicb.cadsr.objectCart.CDECart;
 import gov.nih.nci.ncicb.cadsr.objectCart.CDECartItem;
 import gov.nih.nci.ncicb.cadsr.objectCart.impl.CDECartOCImpl;
 import gov.nih.nci.objectCart.client.ObjectCartClient;
+import gov.nih.nci.objectCart.client.ObjectCartException;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -302,9 +304,9 @@ public class SecureCDECartAction extends FormBuilderSecureBaseDispatchAction {
 		  else
 	    	  cartClient = new ObjectCartClient();
 	      
-	      CDECart userCart = new CDECartOCImpl(cartClient, user.getUsername(),CaDSRConstants.CDE_CART);
-	      
-	      this.setSessionObject(request, CaDSRConstants.CDE_CART, userCart);
+	      ArrayList<CDECart> sessionCarts = CDECartOCImpl.getAllCarts(cartClient, user.getUsername());
+	      sessionCarts = this.nameSort(sessionCarts);
+	      this.setSessionObject(request, CaDSRConstants.CDE_CART, sessionCarts);
 	    }
 	    catch (Exception exp) {
 	      if (log.isErrorEnabled()) {
@@ -330,23 +332,52 @@ public class SecureCDECartAction extends FormBuilderSecureBaseDispatchAction {
    * @throws ServletException
    */
   public ActionForward addItems(
-    ActionMapping mapping,
-    ActionForm form,
-    HttpServletRequest request,
-    HttpServletResponse response) throws IOException, ServletException {
-    try {
-      CDECart cart =
-        (CDECart) this.getSessionObject(request, CaDSRConstants.CDE_CART);
-      saveMessage("cadsr.common.cdecart.save.success",request);
-    }
-    catch (Exception exp) {
-      if (log.isErrorEnabled()) {
-        log.error("Exception on addItems " , exp);
-      }
-    }
+			ActionMapping mapping,
+			ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
 
-    return mapping.findForward("addDeleteSuccess");
-  }
+		try {
+			String userName = getLoggedInUsername(request);
+			ArrayList<CDECart> sessionCarts = (ArrayList<CDECart>) this.getSessionObject(request, CaDSRConstants.CDE_CART);
+			
+			CDECart sessionCart = sessionCarts.get(0);
+			CDECartFormBean myForm = (CDECartFormBean) form;
+			String[] selectedSaveItems = myForm.getSelectedSaveItems();			
+			myForm.setSelectedSaveItems(null);
+			
+			Collection<CDECartItem> items = new ArrayList<CDECartItem>();
+
+			CDEBrowserParams params = CDEBrowserParams.getInstance();
+			String ocURL = params.getObjectCartUrl();			
+			ObjectCartClient ocClient = null;
+			CDECart userCart = null;  
+			if (!ocURL.equals(""))
+				ocClient = new ObjectCartClient(ocURL);
+			else
+				ocClient = new ObjectCartClient();
+
+			userCart = new CDECartOCImpl(ocClient,userName,CaDSRConstants.CDE_CART);			
+
+			for (int i = 0; i < selectedSaveItems.length; i++) {
+				CDECartItem cartItem = sessionCart.findDataElement(selectedSaveItems[i]);
+
+				cartItem.setPersistedInd(true);
+				items.add(cartItem);
+			}      
+			//sessionCart.mergeDataElements(items);      
+			userCart.mergeDataElements(items);
+
+			saveMessage("cadsr.cdecart.save.success",request);
+		}catch (ObjectCartException oce) {
+			if (log.isErrorEnabled()) {
+				log.error("Exception on addItems " , oce);
+			}
+			//saveMessage(exp.getErrorCode(), request);      
+		}
+
+		return mapping.findForward("addDeleteSuccess");
+	}
 
   /**
    * Delete items from the CDE Cart.
@@ -362,38 +393,45 @@ public class SecureCDECartAction extends FormBuilderSecureBaseDispatchAction {
    * @throws ServletException
    */
   public ActionForward removeItems(
-    ActionMapping mapping,
-    ActionForm form,
-    HttpServletRequest request,
-    HttpServletResponse response) throws IOException, ServletException {
-    try {
-      CDECartFormBean myForm = (CDECartFormBean) form;
-      String[] selectedDeleteItems = myForm.getSelectedDeleteItems();
-      Collection savedItems = new ArrayList();
+			ActionMapping mapping,
+			ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		try {
+			String userName = getLoggedInUsername(request);
 
-      //Collection unsavedItems = new ArrayList();
-      Collection items = new ArrayList();
+			CDECartFormBean myForm = (CDECartFormBean) form;
+			String[] selectedDeleteItems = myForm.getSelectedDeleteItems();      
+			Collection items = new ArrayList();
 
-      //Get the cart in the session
-      CDECart sessionCart =
-        (CDECart) this.getSessionObject(request, CaDSRConstants.CDE_CART);
-      CDECartItem item = null;
+			//Get the cart in the session
+			ArrayList<CDECart> sessionCarts = (ArrayList<CDECart>) this.getSessionObject(request, CaDSRConstants.CDE_CART);
+			
+			CDECartItem item = null;
 
-      for (int i = 0; i < selectedDeleteItems.length; i++) {
-        items.add(selectedDeleteItems[i]);
-      }
-      
-      sessionCart.removeDataElements(items);
-      saveMessage("cadsr.common.cdecart.delete.success",request);
-    }
-    catch (Exception exp) {
-      if (log.isErrorEnabled()) {
-        log.error("Exception on removeItems " , exp);
-      }
-    }
+			CDEBrowserParams params = CDEBrowserParams.getInstance();
+			String ocURL = params.getObjectCartUrl();			
+			ObjectCartClient ocClient = null;
+			CDECart userCart = null;  
+			if (!ocURL.equals(""))
+				ocClient = new ObjectCartClient(ocURL);
+			else
+				ocClient = new ObjectCartClient();
 
-    return mapping.findForward("addDeleteSuccess");
-  }
+			userCart = new CDECartOCImpl(ocClient,userName,CaDSRConstants.CDE_CART);
+			sessionCarts = transferDataElements(sessionCarts, selectedDeleteItems, false, userCart);
+			this.setSessionObject(request, CaDSRConstants.CDE_CART, sessionCarts);
+			
+			saveMessage("cadsr.cdecart.delete.success",request);
+		}
+		catch (ObjectCartException oce) {
+			if (log.isErrorEnabled()) {
+				log.error("Exception on removeItems " , oce);
+			}
+			//saveMessage(exp.getErrorCode(), request);      
+		}    
+		return mapping.findForward("addDeleteSuccess");
+	} 
  
  
   public ActionForward subsetQuestionValidValues(
@@ -479,5 +517,205 @@ public class SecureCDECartAction extends FormBuilderSecureBaseDispatchAction {
     }
     return true;
   }  
+  
+  /**
+	 * Adds items to CDE Cart.
+	 *
+	 * @param mapping The ActionMapping used to select this instance.
+	 * @param form The optional ActionForm bean for this request.
+	 * @param request The HTTP Request we are processing.
+	 * @param response The HTTP Response we are processing.
+	 *
+	 * @return
+	 *
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public ActionForward addNewCart(
+			ActionMapping mapping,
+			ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+
+		try {
+			String userName = getLoggedInUsername(request);
+			ArrayList<CDECart> sessionCarts = (ArrayList<CDECart>) this.getSessionObject(request, CaDSRConstants.CDE_CART);
+
+			CDECartFormBean myForm = (CDECartFormBean) form;
+			String[] selectedSaveItems = myForm.getSelectedSaveItems();	
+			String cartName = myForm.getNewCartName();
+			
+			myForm.setSelectedSaveItems(null);
+			
+			Collection<CDECartItem> items = new ArrayList<CDECartItem> ();
+
+			CDEBrowserParams params = CDEBrowserParams.getInstance();
+			String ocURL = params.getObjectCartUrl();			
+			ObjectCartClient ocClient = null;
+			CDECart userCart = null;  
+			if (!ocURL.equals(""))
+				ocClient = new ObjectCartClient(ocURL);
+			else
+				ocClient = new ObjectCartClient();
+
+			userCart = new CDECartOCImpl(ocClient,userName,cartName);			
+			sessionCarts = transferDataElements(sessionCarts, selectedSaveItems, true, userCart);
+			    
+			//sessionCart.mergeDataElements(items);      
+			this.setSessionObject(request, CaDSRConstants.CDE_CART, sessionCarts);
+			
+			//saveMessage("cadsr.cdecart.save.success",request);
+		}catch (ObjectCartException oce) {
+			if (log.isErrorEnabled()) {
+				log.error("Exception on addItems " , oce);
+			}
+			//saveMessage(exp.getErrorCode(), request);      
+		}
+
+		return mapping.findForward("addDeleteSuccess");
+	}
+	
+
+	/**
+	 * Delete CDE Cart.
+	 *
+	 * @param mapping The ActionMapping used to select this instance.
+	 * @param form The optional ActionForm bean for this request.
+	 * @param request The HTTP Request we are processing.
+	 * @param response The HTTP Response we are processing.
+	 *
+	 * @return
+	 *
+	 * @throws IOException
+	 * @throws ServletException
+	 */
+	public ActionForward deleteCart(
+			ActionMapping mapping,
+			ActionForm form,
+			HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+
+		try {
+			String userName = getLoggedInUsername(request);
+			ArrayList<CDECart> sessionCarts = (ArrayList<CDECart>) this.getSessionObject(request, CaDSRConstants.CDE_CART);
+
+			CDECartFormBean myForm = (CDECartFormBean) form;
+			String[] selectedSaveItems = myForm.getSelectedSaveItems();	
+			String cartName = myForm.getDeleteCartName();
+			
+			myForm.setSelectedSaveItems(null);
+			
+			Collection<CDECartItem> items = new ArrayList<CDECartItem> ();
+
+			CDEBrowserParams params = CDEBrowserParams.getInstance();
+			String ocURL = params.getObjectCartUrl();			
+			ObjectCartClient ocClient = null;
+			CDECart userCart = null;  
+			if (!ocURL.equals(""))
+				ocClient = new ObjectCartClient(ocURL);
+			else
+				ocClient = new ObjectCartClient();
+
+			userCart = new CDECartOCImpl(ocClient,userName,cartName);	
+			//Set expiration for Now.
+			userCart.expireCart(new java.util.Date(System.currentTimeMillis()));
+			
+			removeCart(sessionCarts, userCart);
+			          
+			this.setSessionObject(request, CaDSRConstants.CDE_CART, sessionCarts);
+			
+		}catch (ObjectCartException oce) {
+			if (log.isErrorEnabled()) {
+				log.error("Exception on addItems " , oce);
+			}    
+		}
+
+		return mapping.findForward("addDeleteSuccess");
+	}
+
+
+public static ArrayList<CDECart> nameSort(ArrayList<CDECart> carts) {
+		ArrayList<CDECart> ret = new ArrayList<CDECart>();
+		HashMap<String, CDECart> temp = new HashMap<String, CDECart>();
+		ArrayList<String> names = new ArrayList<String>();
+		int i = 0;
+		boolean basicCart = false;
+		boolean fCart = false;
+		for (CDECart cart: carts) {
+			temp.put(cart.getCartName(), cart);
+			if (cart.getCartName().equals(CaDSRConstants.CDE_CART))
+				basicCart = true;
+			else if (cart.getCartName().equals(CaDSRConstants.FORMS_CART))
+				fCart = true;
+			else
+				names.add(cart.getCartName());
+			i++;
+		}
+
+		java.util.Collections.sort(names);
+		
+		//Making sure that the basic cart is always on top of the list.
+		if (basicCart) {
+			names.add(0, CaDSRConstants.CDE_CART);		
+			if (fCart)
+				names.add(1, CaDSRConstants.FORMS_CART);
+		} else if (fCart) 
+			names.add(0, CaDSRConstants.FORMS_CART);
+		
+		for (String name: names) {
+			ret.add(temp.get(name));
+		}
+		
+		return ret;
+	}
+
+	private ArrayList<CDECart> transferDataElements(ArrayList<CDECart> carts, String[] saveItems, boolean persist, CDECart targetCart) {
+		
+		HashMap <String, CDECart> cartMap = new HashMap<String, CDECart>();
+		ArrayList<CDECartItem> ret = new ArrayList<CDECartItem>();
+		for (CDECart cart: carts) {
+			cartMap.put(cart.getCartId(), cart);
+		}
+		
+		for (int i = 0; i < saveItems.length; i++) {
+			String id = saveItems[i];
+			//CartID:DEID
+			String[] split = id.split(":");
+			
+			CDECart cart = cartMap.get(split[0]);
+			CDECartItem ci = cart.findDataElement(split[1]);
+			ci.setPersistedInd(persist);
+			ret.add(ci);
+			cart.removeDataElement(split[1]);
+		}  
+		targetCart.mergeDataElements(ret);
+		
+		for (int i = 0; i < carts.size(); i++) {
+			CDECart cart = carts.get(i);
+			if (cart.getCartId().equals(targetCart.getCartId()))
+				carts.set(i, targetCart);
+		}
+		
+		return carts;
+	}
+	
+	private HashMap<String, CDECart> makeCartMap(ArrayList<CDECart> carts) {
+		HashMap<String, CDECart> cartMap = new HashMap<String, CDECart>();
+		if (carts != null) {
+			for (CDECart c: carts) 
+				cartMap.put(c.getCartName(), c);
+		}
+		return cartMap;
+	}
+	
+	private void removeCart(ArrayList<CDECart> carts, CDECart targetCart) {
+		
+		for (int i = 0; i < carts.size(); i++) {
+			CDECart cart = carts.get(i);
+			if (cart.getCartId().equals(targetCart.getCartId()))
+				carts.remove(i);
+		}
+	}
+  
   
 }
