4876c83d61fe96256a3ac37df0588f6d64e931ab
raleygj <raleygj>
2012-10-24 23:21:11 +0000

    SVN-Revision: 656

--- software/formbuilder/src/java/gov/nih/nci/ncicb/cadsr/formbuilder/common/FormCartHandlingOptionsUtil.java
+++ software/formbuilder/src/java/gov/nih/nci/ncicb/cadsr/formbuilder/common/FormCartHandlingOptionsUtil.java
@@ -7,9 +7,13 @@ public class FormCartHandlingOptionsUtil
 {
 	static private FormCartHandlingOptionsUtil _instance = null;
 
-	// note: these are not mutually exclusive
-	private boolean writeV1Format = true;
-	private boolean writeV2Format = false;
+	// note: write formats are not mutually exclusive
+	private boolean writeV1Format = false;
+	private boolean writeV2Format = true;
+
+	// note: may only be one read format, meant for development/transition use than long-term use
+	private boolean readV2Format = true;
+
 
 	public boolean writeInV1Format()
 	{
@@ -21,16 +25,29 @@ public class FormCartHandlingOptionsUtil
 		return writeV2Format;
 	}
 
+	public boolean readInV1Format()
+	{
+		// read format is controlled by readV2Format
+		return !readV2Format;
+	}
+
+	public boolean readInV2Format()
+	{
+		return readV2Format;
+	}
+
 	protected FormCartHandlingOptionsUtil(){
 		try{
 			InputStream propertyFileStream = this.getClass().getResourceAsStream("/FormCartHandling.properties") ;
 			Properties props = new Properties();
 			props.load(propertyFileStream);
-			String valueV1 = props.getProperty("writeV1Format");
-			String valueV2 = props.getProperty("writeV2Format");
-	
-			writeV1Format = Boolean.valueOf(valueV1);
-			writeV2Format = Boolean.valueOf(valueV2);
+			String value1 = props.getProperty("writeV1Format");
+			String value2 = props.getProperty("writeV2Format");
+			String value3 = props.getProperty("readV2Format");
+
+			writeV1Format = Boolean.valueOf(value1);
+			writeV2Format = Boolean.valueOf(value2);
+			readV2Format = Boolean.valueOf(value3);
 		} 
 		catch(Exception e){
 			System.out.println("FormCartHandlingOptionsUtil error loading properties: " + e);
