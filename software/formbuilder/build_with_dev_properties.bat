4d390d998467bb8c46aa97a8d23b955f5cee5219
raleygj <raleygj>
2012-11-07 21:47:52 +0000

    Reverting to including JDBC driver type in datasource template file instead of datasource URL
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/tags/temp@661 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/formbuilder/build_with_dev_properties.bat
+++ software/formbuilder/build_with_dev_properties.bat
@@ -1,4 +1,4 @@
-ant build-all -DCADSR.DS.URL="jdbc:oracle:thin:@ncidb-dsr-d:1551:DSRDEV" -DCADSR.DS.USER=FORMBUILDER -DCADSR.DS.PSWD=formbuilder
+ant build-all -DCADSR.DS.URL="ncidb-dsr-d:1551:DSRDEV" -DCADSR.DS.USER=FORMBUILDER -DCADSR.DS.PSWD=formbuilder
 
 
 
