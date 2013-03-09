73fed9e6e0227245e39b4d674429e73eeebbad75
raleygj <raleygj>
2013-03-09 00:04:58 +0000

    Update for iteration 2
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@730 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- tools/cart-migration/src/java/converter/FormCartConverter.java
+++ tools/cart-migration/src/java/converter/FormCartConverter.java
@@ -6,6 +6,7 @@ package converter;
 import org.apache.log4j.Logger;
 import gov.nih.nci.ncicb.cadsr.common.CaDSRConstants;
 import gov.nih.nci.ncicb.cadsr.common.resource.Form;
+import gov.nih.nci.ncicb.cadsr.common.resource.FormV2;
 
 import gov.nih.nci.ncicb.cadsr.objectCart.CDECart;
 import gov.nih.nci.ncicb.cadsr.objectCart.CDECartItem;
@@ -17,6 +18,21 @@ import gov.nih.nci.system.applicationservice.ApplicationException;
 import gov.nih.nci.system.client.ApplicationServiceProvider;
 import gov.nih.nci.objectCart.applicationService.ObjectCartService;
 import gov.nih.nci.objectCart.domain.Cart;
+
+import gov.nih.nci.ncicb.cadsr.formbuilder.common.FormBuilderException;
+import org.exolab.castor.xml.MarshalException;
+import org.exolab.castor.xml.ValidationException;
+import javax.xml.transform.TransformerException;
+
+import gov.nih.nci.ncicb.cadsr.formbuilder.ejb.FormBuilder;
+import gov.nih.nci.ncicb.cadsr.formbuilder.ejb.FormBuilderHome;
+import javax.naming.InitialContext;
+import javax.naming.Context;
+import java.util.Properties;
+import java.rmi.RemoteException;
+import javax.rmi.PortableRemoteObject;
+
+import org.jnp.interfaces.NamingContextFactory;
 
 import gov.nih.nci.ncicb.cadsr.formbuilder.struts.common.FormConverterUtil;
 
@@ -34,6 +50,7 @@ public class FormCartConverter {
 	private String objectCartURL;
 	private ObjectCartService cartService;
 	private ObjectCartClient cartClient;
+	private FormBuilder formBuilderService;
 	
 	private int exceptionsWhileClearingExisting = 0;
 	private int exceptionsWhileConverting = 0;
@@ -43,7 +60,7 @@ public class FormCartConverter {
 	private boolean limitToGuest; // limit changes to the guest cart (if changes are allowed)
 	private boolean noChanges;
 
-	public FormCartConverter(String ocURL, boolean limit, boolean guestOnly) {
+	public FormCartConverter(String formbuilderURL, String username, String password, String ocURL, boolean limit, boolean guestOnly) {
 		try {
 			objectCartURL = ocURL;
 			noChanges = limit;
@@ -63,7 +80,26 @@ public class FormCartConverter {
 			
 		} catch (Exception e) {
 			throw new RuntimeException("FormCartConverterfailed to create ObjectCart service/client ", e);
-		}		
+		}
+		
+		try {
+			Properties env = new Properties();
+			env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jnp.interfaces.NamingContextFactory");
+			env.put(Context.URL_PKG_PREFIXES, "org.jboss.naming:org.jnp.interfaces");		
+			env.put(Context.PROVIDER_URL, formbuilderURL);			
+			env.put(Context.SECURITY_PRINCIPAL, username);
+			env.put(Context.SECURITY_CREDENTIALS, password);
+			Context context = new InitialContext(env);
+			Object remote = context.lookup("FormBuilder");
+			FormBuilderHome home = (FormBuilderHome)PortableRemoteObject.narrow(remote,FormBuilderHome.class);	
+			formBuilderService = home.create();
+			String testConnection = formBuilderService.getIdseq((int)1, (float)0.1); // ("" instead of exception when no matching form)
+			_logger.info("testConnection returned: " + testConnection);
+			_logger.info("Form Builder service created using URL " + formbuilderURL + " for username " + username);
+		} catch (Exception e) {
+			throw new RuntimeException("FormCartConverter failed to create FormBuilder service ", e);
+		}
+				
 	}
 
 	public static void main(String[] args_) {
@@ -73,27 +109,33 @@ public class FormCartConverter {
 			_logger.info("started main FormCartConverter");
 
 			int argsCount = args_.length;
-			if (argsCount < 1 || argsCount > 2) {
+			if (argsCount < 4 || argsCount > 5) {
 				_logger.info("Incorrect number of arguments.");				
-				_logger.info("First argument is the object cart url.");
-				_logger.info("Optional second argument may be blank, 'guest', or 'all' to specify which carts to operate on (blank is none)");
-				_logger.info("e.g.: java -Xmx512M -jar FormCartConverter.jar https://objcart-dev.nci.nih.gov/objcart10/ all");
+				_logger.info("First argument is the Form Builder URL for EJB use.");				
+				_logger.info("Second argument is a username to access Form Builder EJB.");				
+				_logger.info("Third argument is the password to access Form Builder EJB.");				
+				_logger.info("Fourth argument is the object cart url.");
+				_logger.info("Optional fifth argument may be blank, 'guest', or 'all' to specify which carts to operate on (blank is none)");
+				_logger.info("e.g.: java -Xmx512M -jar FormCartConverter.jar jnp://127.0.0.1:1099 user password https://objcart-dev.nci.nih.gov/objcart10/ all");
 			} else {
 			
-				String ocURL = args_[0];
+				String formbuilderURL = args_[0];
+				String username = args_[1];
+				String password = args_[2];
+				String ocURL = args_[3];
 				
 				boolean limit = true;
 				boolean guestOnly = true;
-				if (argsCount == 2 && args_[1].equalsIgnoreCase("guest")) {
+				if (argsCount == 5 && args_[4].equalsIgnoreCase("guest")) {
 					limit = false;
 					guestOnly = true;
 				}
-				if (argsCount == 2 && args_[1].equalsIgnoreCase("all")) {
+				if (argsCount == 5 && args_[4].equalsIgnoreCase("all")) {
 					limit = false;
 					guestOnly = false;
 				}
 				
-				FormCartConverter converter = new FormCartConverter(ocURL, limit, guestOnly);
+				FormCartConverter converter = new FormCartConverter(formbuilderURL, username, password, ocURL, limit, guestOnly);
 				
 				int clearingErrors = converter.clearExistingV2FormCarts();
 				if (clearingErrors == 0) {
@@ -191,7 +233,7 @@ public class FormCartConverter {
 
 				} catch (Exception e) {
 					exceptionsWhileConverting++;
-					_logger.debug("  exception converting cart: " + e.getMessage());
+					_logger.debug("  exception converting cart: msg: " + e.getMessage() + " toString: " + e.toString());			
 				}	
 
 			}
@@ -215,7 +257,7 @@ public class FormCartConverter {
 	}
 
 
-	private Cart addFormsToV2Cart(Collection forms, String userId) throws ObjectCartException {
+	private Cart addFormsToV2Cart(Collection forms, String userId) throws ObjectCartException, FormBuilderException, RemoteException, MarshalException, ValidationException, TransformerException {
 		// Loosely based on FormAction addFormToCart
 		// We don't check for duplicates -- we assume the v2 cart is empty and that added forms don't include duplicates 	
 
@@ -234,16 +276,16 @@ public class FormCartConverter {
 	}
 
 	
-	private CartObject translateCartObject(Form crf) {
-		// function copied from FormAction without modification
+	private CartObject translateCartObject(Form crf) throws FormBuilderException, RemoteException, MarshalException, ValidationException, TransformerException {
+		// function copied from FormAction and then modified for formV2
 		CartObject ob = new CartObject();
 		ob.setType(FormConverterUtil.instance().getCartObjectType());
 		ob.setDisplayText(Integer.toString(crf.getPublicId()) + "v" + Float.toString(crf.getVersion()));
-		ob.setNativeId(crf.getFormIdseq());
-
-		String convertedForm = FormConverterUtil.instance().convertFormToV2(crf);		
+		ob.setNativeId(crf.getFormIdseq());		
+		FormV2 formV2 = formBuilderService.getFormDetailsV2(crf.getFormIdseq());	
+		String convertedForm = FormConverterUtil.instance().convertFormToV2(formV2);
 		ob.setData(convertedForm);
-		return ob;	  
+		return ob;
 	}	
 
 	public int getNumberOfCartsConverted() {
