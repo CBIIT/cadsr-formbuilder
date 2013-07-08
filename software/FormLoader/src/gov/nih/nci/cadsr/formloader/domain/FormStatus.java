f8fc9826b4efcc728fe87a5fc1a1e058e14fa6ba
Shan Yang <yangs8@mail.nih.gov>
2013-07-08 14:32:07 +0000

    Rename from Status
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@764 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

---
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/domain/FormStatus.java
@@ -0,0 +1,32 @@
+package gov.nih.nci.cadsr.formloader.domain;
+
+import java.util.List;
+
+public class FormStatus {
+	
+	public static final String STATUS_XML_VALIDATED = "XML_VALIDATED"; 
+	public static final String STATUS_DB_VALIDATED = "DB_VALIDATED";
+	public static final String STATUS_LOADED = "LOADED";
+	public static final String STATUS_UNLOADED = "UNLOADED";
+	public static final String STATUS_ERROR = "ERROR";
+	
+	private String type;
+	private List<String> messages;
+	
+	public FormStatus(String type) {
+		this.type = type;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public List<String> getMessages() {
+		return messages;
+	}
+	public void setMessages(List<String> messages) {
+		this.messages = messages;
+	}
+
+}
