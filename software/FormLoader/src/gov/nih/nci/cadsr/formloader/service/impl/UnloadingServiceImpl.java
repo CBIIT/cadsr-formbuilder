d7b64185505a989c3f9c7289d3611f3c20d3cccb
Shan Yang <yangs8@mail.nih.gov>
2013-07-23 19:24:33 +0000

    reference FormDescriptor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@782 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/impl/UnloadingServiceImpl.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/impl/UnloadingServiceImpl.java
@@ -1,12 +1,10 @@
 package gov.nih.nci.cadsr.formloader.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 import gov.nih.nci.cadsr.formloader.domain.FormCollection;
-import gov.nih.nci.cadsr.formloader.domain.FormHeader;
+import gov.nih.nci.cadsr.formloader.domain.FormDescriptor;
 import gov.nih.nci.cadsr.formloader.service.UnloadingService;
 import gov.nih.nci.cadsr.formloader.service.common.FormLoaderServiceException;
 
@@ -21,7 +19,7 @@ public class UnloadingServiceImpl implements UnloadingService {
 	}
 
 	@Override
-	public List<FormHeader> unloadForms(List<FormHeader> forms)
+	public List<FormDescriptor> unloadForms(List<FormDescriptor> forms)
 			throws FormLoaderServiceException {
 		// TODO Auto-generated method stub
 		return null;
