8fe452a7bfcf36e9b171e92dac5bb87ad52c34ac
Shan Yang <yangs8@mail.nih.gov>
2013-07-23 19:20:36 +0000

    Interface changes - mehtods throw FormLoaderServiceException
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@777 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/XmlValidationService.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/XmlValidationService.java
@@ -1,13 +1,13 @@
 package gov.nih.nci.cadsr.formloader.service;
 
-import gov.nih.nci.cadsr.formloader.domain.FormHeader;
+import gov.nih.nci.cadsr.formloader.domain.FormDescriptor;
 import gov.nih.nci.cadsr.formloader.service.common.FormLoaderServiceException;
 
 import java.util.List;
 
 public interface XmlValidationService {
 	
-	public List<FormHeader> validateXml(String xmlPathName) 
+	public List<FormDescriptor> validateXml(String xmlPathName) 
 			throws FormLoaderServiceException;
 
 }
