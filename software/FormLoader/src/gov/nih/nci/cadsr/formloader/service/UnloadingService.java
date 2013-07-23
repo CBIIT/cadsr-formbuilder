af39cd2f363177fc8e6761b25a4e0a52fbc105a6
Shan Yang <yangs8@mail.nih.gov>
2013-07-23 19:19:45 +0000

    Interface changes
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@776 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/UnloadingService.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/UnloadingService.java
@@ -3,13 +3,13 @@ package gov.nih.nci.cadsr.formloader.service;
 import java.util.List;
 
 import gov.nih.nci.cadsr.formloader.domain.FormCollection;
-import gov.nih.nci.cadsr.formloader.domain.FormHeader;
+import gov.nih.nci.cadsr.formloader.domain.FormDescriptor;
 import gov.nih.nci.cadsr.formloader.service.common.FormLoaderServiceException;
 
 public interface UnloadingService {
 	public List<FormCollection> unloadCollections(List<FormCollection> collections) 
 			throws FormLoaderServiceException;
 	
-	public List<FormHeader> unloadForms(List<FormHeader> forms) throws FormLoaderServiceException;
+	public List<FormDescriptor> unloadForms(List<FormDescriptor> forms) throws FormLoaderServiceException;
 
 }
