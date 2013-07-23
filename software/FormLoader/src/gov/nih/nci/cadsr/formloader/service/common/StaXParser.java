19f2f04b22ce2c03dafde94021c3dac8c98a488b
Shan Yang <yangs8@mail.nih.gov>
2013-07-23 19:21:24 +0000

    Restructured to take various types of parser handlers
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@778 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/common/StaXParser.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/common/StaXParser.java
@@ -1,16 +1,16 @@
 package gov.nih.nci.cadsr.formloader.service.common;
 
-import gov.nih.nci.cadsr.formloader.domain.FormHeader;
+import gov.nih.nci.cadsr.formloader.domain.FormDescriptor;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamConstants;
@@ -22,38 +22,66 @@ import org.apache.log4j.Logger;
 public class StaXParser {
 	
 	private static Logger logger = Logger.getLogger(StaXParser.class.getName());
+	
+	protected static final Map<String, String> MAP_XML_ELEM_TO_DTO_PROPERTY = createMap();
+	
+	protected static final String PREFIX_CDE = "cde";
+
+    protected static Map<String, String> createMap() {
+        Map<String, String> mapping = new HashMap<String, String>();
+        mapping.put("publicID", "publicId");
+        mapping.put(PREFIX_CDE+"publicID", "cdePublicId");
+        mapping.put(PREFIX_CDE+"version", "cdeVersion");
+       return Collections.unmodifiableMap(mapping);
+    }
 
 	//Mapping node name in xml
-	private static final String FORM = "form";
-	private static final String PUBLIC_ID = "publicID";
-	private static final String LONG_NAME = "longName";
-	private static final String VERSION = "version";
-	private static final String CONTEXT = "context";
-	private static final String WORKFLOW_STATUS = "workflowStatusName";
-	private static final String TYPE = "type";
-	private static final String PROTOCOL = "protocol";
-	private static final String MODULE = "module";
-
-	List<FormHeader> generatedForms = new ArrayList<FormHeader>();
+	protected static final String FORM = "form";
+	protected static final String PUBLIC_ID = "publicID";
+	protected static final String LONG_NAME = "longName";
+	protected static final String VERSION = "version";
+	protected static final String CONTEXT = "context";
+	protected static final String WORKFLOW_STATUS = "workflowStatusName";
+	protected static final String TYPE = "type";
+	protected static final String PROTOCOL = "protocol";
+	protected static final String MODULE = "module";	
 	
-	FormHeader tempForm;
-	int formCount = 0;
-	int moduleCountForForm = 0;	
-	String methodName;
+	//Question 
+	protected static final String QUESTION = "question";
+	protected static final String QUESTION_TEXT = "questionText";
+	protected static final String DEFAULT_VALUE = "defaultValue";
+	protected static final String INSTRUCTION = "instruction";
+	protected static final String TEXT = "text";
+	protected static final String VALID_VALUE = "validValue";
+	protected static final String VALUE = "value";
+	protected static final String MEANING_TEXT = "meaningText";
 	
-	ArrayDeque<String> nodeQueue;
+	protected static final String DATA_ELEMENT = "dataElement";
+	protected static final String VALUE_DOMAIN = "valueDomain";
 	
-	private void reset() {
-		generatedForms.clear();
-		formCount = 0;
-		moduleCountForForm = 0;
+	protected static final String REFERENCE_DOCUMENT = "referenceDocument";
+	protected static final String REFERENCE_DOCUMENT_TYPE = "type";
+	protected static final String REFERENCE_DOCUMENT_DOCTEXT = "doctext";
+	
+	
+	
+	public List<FormDescriptor> parseFormHeaders(String xmlPathName) {
+		List<FormDescriptor> forms = new ArrayList<FormDescriptor>();
+		ParserHandler handler = new FormParserHandler(forms);
+		return parseFormHeaders(xmlPathName, handler);
+	}
+	
+	public List<FormDescriptor> parseFormQuestions(String xmlPathName, List<FormDescriptor> forms) {
+		if (forms == null || forms.size() == 0) {
+			logger.error("No forms to work on. Should call parseFormHeaders() first");
+			return null;
+		}
 		
-		nodeQueue = new ArrayDeque<String>();
+		ParserHandler handler = new FormQuestionParserHandler(forms);
+		return parseFormHeaders(xmlPathName, handler);
 	}
 
-	public List<FormHeader> parseFormHeaders(String xmlPathName) {
-		
-		reset();
+	public List<FormDescriptor> parseFormHeaders(String xmlPathName, ParserHandler handler) {
 		
 		FileReader fReader = null;
 		BufferedReader bReader = null;
@@ -66,7 +94,7 @@ public class StaXParser {
 			
 			while(xmlreader.hasNext()){
 				
-			      readNext(xmlreader);
+			      readNext(xmlreader, handler);
 			      xmlreader.next();
 			}
 			      
@@ -87,13 +115,14 @@ public class StaXParser {
 			}
 		}
 		
+		List<FormDescriptor> generatedForms = handler.getFormList();
 		
 		for (int i = 0; i < generatedForms.size(); i++) 
 			logger.debug(generatedForms.get(i).toString());
 		return generatedForms;
 	}
 	
-	protected void readNext(XMLStreamReader xmlreader) {
+	protected void readNext(XMLStreamReader xmlreader,ParserHandler handler) {
 		if (xmlreader == null) {
 			logger.debug("xmlread is null - this should never happen.");
 			return;
@@ -102,13 +131,13 @@ public class StaXParser {
 		switch (xmlreader.getEventType()) {
 
 		case XMLStreamConstants.START_ELEMENT:
-			handleStartElement(xmlreader);
+			handler.handleStartElement(xmlreader);
 			break;
 		case XMLStreamConstants.END_ELEMENT:
-			handleEndElement(xmlreader);
+			handler.handleEndElement(xmlreader);
 			break;
 		case XMLStreamConstants.CHARACTERS:
-			handleCharacterElement(xmlreader);
+			handler.handleCharacterElement(xmlreader);
 			break;
 		case XMLStreamConstants.PROCESSING_INSTRUCTION:
 			System.out.print("Processing Instrcutions\n");
@@ -139,141 +168,7 @@ public class StaXParser {
 			break;
 		}
 
-
 	}
 
 
-	protected void handleStartElement(XMLStreamReader xmlreader) {
-		
-		if (xmlreader == null) {
-			logger.debug("xmlreader is null!!!! Do nothing");
-			return;
-		}
-			
-		if (!xmlreader.hasName()) {
-			logger.debug("Got an elem without name. Do nothing");
-			return;
-		}
-		
-		String localName = xmlreader.getLocalName();
-		
-		if (localName.equals(StaXParser.FORM)) {
-			
-			int lineNum = xmlreader.getLocation().getLineNumber();
-			logger.debug("Got a form at line: " + lineNum);
-			tempForm = new FormHeader();
-			tempForm.setXml_line_begin(lineNum);
-			this.methodName = null;
-			formCount++;
-		} else if (localName.equals(StaXParser.LONG_NAME) || 
-				localName.equals(StaXParser.CONTEXT) || 
-				localName.equals(StaXParser.VERSION)  ||
-				localName.equals(StaXParser.TYPE) ||
-				localName.equals(StaXParser.WORKFLOW_STATUS) ||
-				localName.equalsIgnoreCase(StaXParser.PUBLIC_ID)) {
-			if (nodeQueue.peek().equals(StaXParser.FORM)) {
-				this.methodName = getMethodName(localName);	
-			} else if (nodeQueue.peek().equals(StaXParser.PROTOCOL) &&
-					localName.equals(StaXParser.LONG_NAME)) {
-				this.methodName = getMethodName(StaXParser.PROTOCOL) + "Name"; 
-				
-			}
-		
-		} else if (localName.equals(StaXParser.MODULE)) {
-			tempForm.setNumberOfModules(tempForm.getNumberOfModules() + 1);
-		} 
-		
-		//logger.debug("Pushing to node queue: " + localName);
-		this.nodeQueue.push(localName);
-	}
-	
-	protected void handleCharacterElement(XMLStreamReader xmlreader) {
-		if (xmlreader == null) {
-			logger.debug("xmlreader is null!!!! Do nothing");
-			return;
-		}
-		
-		String currNode = (nodeQueue.peek() != null) ? nodeQueue.peek().toLowerCase() : null;
-		
-		if (currNode != null && methodName != null) {
-			logger.debug("Queue head: " + nodeQueue.peek() + " | method name: " + methodName + " | value to set: " + xmlreader.getText());
-			setFormProperty(methodName, xmlreader.getText());
-		}
-	}
-	
-	protected void setFormProperty(String methodName, String value) {
-		//String parameter
-		try {
-			Class[] paramString = new Class[1];	
-			paramString[0] = String.class;
-			Method method = tempForm.getClass().getSuperclass().getDeclaredMethod(methodName, paramString);
-			method.invoke(tempForm, value);
-			
-		} catch (NoSuchMethodException nsme) {
-			logger.debug(nsme);
-		} catch (SecurityException se) {
-			logger.debug(se);
-		} catch (IllegalArgumentException ex) {
-            ex.printStackTrace();
-        } catch (InvocationTargetException ex) {
-            ex.printStackTrace();
-            return;
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-            return;
-        } catch(Exception ex) {
-			logger.debug(ex);
-    	} catch (Throwable t) {
-    		logger.debug(t);
-    	}
-
-	}
-	
-	protected void handleEndElement(XMLStreamReader xmlreader) {
-		
-		if (xmlreader == null) {
-			logger.debug("xmlreader is null!!!! Do nothing");
-			return;
-		}
-			
-		if (!xmlreader.hasName()) {
-			logger.debug("Got an elem without name. Do nothing");
-			return;
-		}
-		
-		String localName = xmlreader.getLocalName();
-		
-		if (localName.equals(StaXParser.FORM)) {
-			int lineNum = xmlreader.getLocation().getLineNumber();
-			logger.debug("End parsing a form at line: " + lineNum);
-			 tempForm.setXml_line_end(lineNum);
-			 logger.debug(tempForm.toString());
-			 generatedForms.add(tempForm);
-			 
-			 logger.debug("peak: " + (String)nodeQueue.peek());
-			 logger.debug("peak first: " + (String)nodeQueue.peekFirst());
-			 logger.debug("peak last: " + (String)nodeQueue.peekLast());
-			 
-		} 
-		
-		this.methodName = null;
-		if (localName.equals(StaXParser.FORM))
-			nodeQueue.clear();
-		else {
-			if (!nodeQueue.isEmpty())
-				nodeQueue.pop();
-		}
-	}
-	
-	/**
-	 * 
-	 * @param xmlElemName assumption for xmlElemName format: Camel case with 1st letter in first word in lower case
-	 * @return
-	 */
-	protected String getMethodName(String xmlElemName) {
-					
-		char[] stringArray = xmlElemName.toCharArray();
-		stringArray[0] = Character.toUpperCase(stringArray[0]);
-		return "set" + new String(stringArray);
-	}
 }
