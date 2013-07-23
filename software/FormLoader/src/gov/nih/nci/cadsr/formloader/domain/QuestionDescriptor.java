7b2c2d81d068514e03ea09380b05c289027f1b53
Shan Yang <yangs8@mail.nih.gov>
2013-07-23 16:33:31 +0000

    init checkin
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@772 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

---
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/domain/QuestionDescriptor.java
@@ -0,0 +1,178 @@
+package gov.nih.nci.cadsr.formloader.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionDescriptor {
+	
+	String questionSeqId;
+	String publicId;
+	String version;
+	
+	String questionText;
+	
+	String cdeSeqId;
+	String cdePublicId;
+	String cdeVersion;
+	
+	List<ValidValue> validValues = new ArrayList<ValidValue>();
+	String defaultValue;
+	
+	String instruction;
+	
+	boolean skip = false;
+	
+	public String getQuestionSeqId() {
+		return questionSeqId;
+	}
+
+
+
+	public void setQuestionSeqId(String questionSeqId) {
+		this.questionSeqId = questionSeqId;
+	}
+
+
+
+	public String getPublicId() {
+		return publicId;
+	}
+
+
+
+	public void setPublicId(String publicId) {
+		this.publicId = publicId;
+	}
+
+
+
+	public String getVersion() {
+		return version;
+	}
+
+
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+
+
+	public String getQuestionText() {
+		return questionText;
+	}
+
+
+
+	public void setQuestionText(String questionText) {
+		this.questionText = questionText;
+	}
+
+
+
+	public String getCdeSeqId() {
+		return cdeSeqId;
+	}
+
+
+
+	public void setCdeSeqId(String cdeSeqId) {
+		this.cdeSeqId = cdeSeqId;
+	}
+
+
+
+	public String getCdePublicId() {
+		return cdePublicId;
+	}
+
+
+
+	public void setCdePublicId(String cdePublicId) {
+		this.cdePublicId = cdePublicId;
+	}
+
+
+
+	public String getCdeVersion() {
+		return cdeVersion;
+	}
+
+
+
+	public void setCdeVersion(String cdeVersion) {
+		this.cdeVersion = cdeVersion;
+	}
+
+
+
+	public List<ValidValue> getValidValues() {
+		return validValues;
+	}
+
+
+
+	public void setValidValues(List<ValidValue> validValues) {
+		this.validValues = validValues;
+	}
+
+
+
+	public String getDefaultValue() {
+		return defaultValue;
+	}
+
+
+
+	public void setDefaultValue(String defaultValue) {
+		this.defaultValue = defaultValue;
+	}
+
+
+
+	public String getInstruction() {
+		return instruction;
+	}
+
+
+
+	public void setInstruction(String instruction) {
+		this.instruction = instruction;
+	}
+
+	public void addInstruction(String newInstruction) {
+		if (instruction == null)
+			instruction = newInstruction;
+		else
+			instruction += ";" + newInstruction;
+	}
+
+	public boolean isSkip() {
+		return skip;
+	}
+
+	public void setSkip(boolean skip) {
+		this.skip = skip;
+	}
+
+
+
+	public class ValidValue {
+		String value;
+		String meaningText;
+		public String getValue() {
+			return value;
+		}
+		public void setValue(String value) {
+			this.value = value;
+		}
+		public String getMeaningText() {
+			return meaningText;
+		}
+		public void setMeaningText(String valueMeaning) {
+			this.meaningText = valueMeaning;
+		}
+		
+	}
+
+}
