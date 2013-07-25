cdc0b57237a3529bd8351904b0cfcd8eb58cfff7
Shan Yang <yangs8@mail.nih.gov>
2013-07-25 20:16:28 +0000

    Moved load status constants to FormStatus
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@800 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/domain/FormStatus.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/domain/FormStatus.java
@@ -4,22 +4,13 @@ import java.util.List;
 
 public class FormStatus {
 	
-	public static final String STATUS_XML_VALIDATED = "XML_VALIDATED"; 
-	public static final String STATUS_DB_VALIDATED = "DB_VALIDATED";
-	public static final String STATUS_LOADED = "LOADED";
-	public static final String STATUS_UNLOADED = "UNLOADED";
-	public static final String STATUS_ERROR = "ERROR";
-	
-	protected String type;
+	protected int type;
 	protected List<String> messages;
 	
-	public FormStatus(String type) {
-		this.type = type;
-	}
-	public String getType() {
+	public int getType() {
 		return type;
 	}
-	public void setType(String type) {
+	public void setType(int type) {
 		this.type = type;
 	}
 	public List<String> getMessages() {
