5f44986d8cd06cbce860442ff470b7beac19383e
Shan Yang <yangs8@mail.nih.gov>
2013-07-25 20:18:52 +0000

    Added more error code.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@802 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/common/FormLoaderServiceException.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/common/FormLoaderServiceException.java
@@ -3,9 +3,18 @@ package gov.nih.nci.cadsr.formloader.service.common;
 
 public class FormLoaderServiceException extends Exception {
 	
+	public static final int ERROR_NO_ERROR = 0;
+	public static final int ERROR_MALFORMED_XML = 1;
+	public static final int ERROR_EMPTY_FORM_LIST = 2;
+	
 	protected int errorCode;
 	
 	FormLoaderServiceError error; 
+	
+	public  FormLoaderServiceException(int errorCode, String message) {
+		super(message);
+		this.errorCode = errorCode;
+	}
 	
 	public FormLoaderServiceException(int errorCode, String message, FormLoaderServiceError error)  {
 		super(message);
