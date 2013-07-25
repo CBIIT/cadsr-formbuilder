fbdee0f18511420a54297974ccbb6a093f03fa13
Shan Yang <yangs8@mail.nih.gov>
2013-07-25 20:16:59 +0000

    Added more dao calls for content validation
    git-svn-id: https://ncisvn.nci.nih.gov/svn/formbuilder/trunk@801 e2a2ee12-66c9-4113-8937-11a3f5b2fde5

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/repository/FormLoaderRepository.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/repository/FormLoaderRepository.java
@@ -8,14 +8,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import gov.nih.nci.cadsr.formloader.domain.FormDescriptor;
+import gov.nih.nci.ncicb.cadsr.common.dto.DataElementTransferObject;
 import gov.nih.nci.ncicb.cadsr.common.dto.ModuleTransferObject;
 import gov.nih.nci.ncicb.cadsr.common.dto.QuestionTransferObject;
 import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCFormDAOV2;
 import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCModuleDAOV2;
 import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCQuestionDAOV2;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCValueDomainDAOV2;
 import gov.nih.nci.ncicb.cadsr.common.resource.FormV2;
 import gov.nih.nci.ncicb.cadsr.common.resource.Module;
+import gov.nih.nci.ncicb.cadsr.common.resource.PermissibleValueV2;
 import gov.nih.nci.ncicb.cadsr.common.resource.ReferenceDocument;
+import gov.nih.nci.ncicb.cadsr.common.resource.ValueDomainV2;
 
 @Repository
 public class FormLoaderRepository {
@@ -30,6 +34,8 @@ public class FormLoaderRepository {
 	@Autowired
 	JDBCQuestionDAOV2 questionV2Dao;
 	
+	@Autowired
+	JDBCValueDomainDAOV2 valueDomainV2Dao;
 	
 
 	/**
@@ -73,24 +79,85 @@ public class FormLoaderRepository {
 		return questions;
 	}
 	
+	public List<QuestionTransferObject> getQuestionsByPublicId(String publicId) {
+		if (publicId == null || publicId.length() == 0) {
+			logger.debug("Question public id is null or empty. Unable to querry db.");
+			return null;
+		}
+		
+		int pubId = Integer.parseInt(publicId);
+		
+		List<QuestionTransferObject> questions = questionV2Dao.getQuestionsByPublicId(pubId);
+		logger.debug("Dao returns " + questions.size() + " questions.");
+		return questions;
+	}
+	
+	public List<QuestionTransferObject> getQuestionsByPublicIdAndVersion(String publicId, String version) {
+		if (publicId == null || publicId.length() == 0) {
+			logger.debug("Question public id is null or empty. Unable to querry db.");
+			return null;
+		}
+		
+		if (version == null || version.length() == 0) {
+			logger.debug("Question version is null or empty. Unable to querry db.");
+			return null;
+		}
+		
+		int pubId = Integer.parseInt(publicId);
+		float vers = Float.parseFloat(version);
+		List<QuestionTransferObject> questions = questionV2Dao.getQuestionByPublicIdAndVersion(pubId, vers);
+		logger.debug("Dao returns " + questions.size() + " questions.");
+		return questions;
+	}
+	
+	public List<DataElementTransferObject> getCDEByPublicId(String cdePublicId) {
+		if (cdePublicId == null || cdePublicId.length() == 0) {
+			logger.debug("Question's CDE public id is null or empty. Unable to querry db.");
+			return null;
+		}
+		
+		List<DataElementTransferObject> des = questionV2Dao.getCdesByPublicId(cdePublicId);
+		logger.debug("Dao returns " + des.size() + " CDEs");
+		
+		return des;
+	}
+	
 	public List<ReferenceDocument> getReferenceDocsForQuestionCde(String cdePublicId, String cdeVersion) {
+		if (cdePublicId == null || cdePublicId.length() == 0) {
+			logger.debug("Question's CDE public id is null or empty. Unable to querry db.");
+			return null;
+		}
+		
+		if (cdeVersion == null || cdeVersion.length() == 0) {
+			logger.debug("Question CDE version is null or empty. Unable to querry db.");
+			return null;
+		}
 		
 		List<ReferenceDocument> deRefDocs = questionV2Dao.getAllReferenceDocuments(cdePublicId, cdeVersion);
-			   
+		logger.debug("Dao returns " + deRefDocs.size() + " CDE reference docs.");
 		
-		//  getReferenceDocuments(term.getDataElement().getDeIdseq());
-			    // term.getDataElement().setReferenceDocs(deRefDocs);
-		
-	/*	
-		private List<ReferenceDocument> getReferenceDocuments(String acIdseq)
-	    {
-	        ReferenceDocumentDAO myDAO = (ReferenceDocumentDAO)daoFactory.getReferenceDocumentDAO();
-	        List refDocs = myDAO.getAllReferenceDocuments(acIdseq, 
-	                        null);
-	        return refDocs;        
-	    }
-		*/	     
 		return deRefDocs;
+	}
+	
+	public ValueDomainV2 getValueDomainBySeqId(String vdSeqId) {
+		if (vdSeqId == null || vdSeqId.length() == 0) {
+			logger.debug("Value domain seq id is null or empty. Unable to querry db.");
+			return null;
+		}
+		
+		ValueDomainV2 vd = valueDomainV2Dao.getValueDomainV2ById(vdSeqId);
+		String msg = "Dao returns ";
+		if (vd == null)
+			msg += "null ValueDomainV2 obj";
+		else {
+			List<PermissibleValueV2> pValues = vd.getPermissibleValueV2();
+			if (pValues == null || pValues.size() == 0)
+				msg += "a value domain obj with 0 permissible value.";
+			else
+				msg += "a value domain obj with " + pValues.size() + " permissible values";
+		}
+		logger.debug(msg);
+		return vd;
 	}
 	
 	 
