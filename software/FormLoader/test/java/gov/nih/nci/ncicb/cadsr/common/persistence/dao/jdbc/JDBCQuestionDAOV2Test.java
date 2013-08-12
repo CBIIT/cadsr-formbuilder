5edc14a56883e8c7ad788294ed3047c17fd841ea
shanyang-saicf <shan.yang@nih.gov>
2013-08-12 13:44:31 -0400

    Spring config file change

--- software/FormLoader/test/java/gov/nih/nci/ncicb/cadsr/common/persistence/dao/jdbc/JDBCQuestionDAOV2Test.java
+++ software/FormLoader/test/java/gov/nih/nci/ncicb/cadsr/common/persistence/dao/jdbc/JDBCQuestionDAOV2Test.java
@@ -15,7 +15,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/applicationContext-jdbcdao-test.xml"})
+@ContextConfiguration(locations = {"classpath:/applicationContext-service-test-db.xml"})
 public class JDBCQuestionDAOV2Test {
 	
 	@Autowired
