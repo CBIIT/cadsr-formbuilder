795d821a3029082838f2c1ebb12bef67d055eca4
Sula Rajapakse <sulalith@gmail.com>
2013-08-25 09:06:38 -0400

    Second push for FL gui code. changes to struts files

---
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/struts2/HomeAction.java
@@ -0,0 +1,13 @@
+package gov.nih.nci.cadsr.formloader.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HomeAction extends ActionSupport {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+}
---
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/struts2/ValidateXMLAction.java
@@ -0,0 +1,16 @@
+package gov.nih.nci.cadsr.formloader.struts2;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ValidateXMLAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public String execute() {
+		return SUCCESS;
+	}
+
+}
