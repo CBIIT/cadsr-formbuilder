54ec7ac5d62b31e8dddb51523e44e0a8dbd56510
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2013-08-27 16:44:18 -0400

    Completed Unload service

---
+++ software/FormLoader/test/java/gov/nih/nci/cadsr/formloader/service/impl/CollectionRetrievalServiceImplTest.java
@@ -0,0 +1,41 @@
+package gov.nih.nci.cadsr.formloader.service.impl;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import gov.nih.nci.cadsr.formloader.domain.FormCollection;
+import gov.nih.nci.cadsr.formloader.service.common.FormLoaderServiceException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/applicationContext-service-test-db.xml"})
+public class CollectionRetrievalServiceImplTest {
+	
+	@Autowired
+	CollectionRetrievalServiceImpl collectionRetrievalService;
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testGetAllCollections() {
+		try {
+			List<FormCollection> colls = collectionRetrievalService.getAllCollections();
+			assertNotNull(colls);
+			assertTrue(colls.size() > 10);
+		
+		} catch (FormLoaderServiceException e) {
+			fail(e.getMessage());
+		}
+		
+	}
+
+}
