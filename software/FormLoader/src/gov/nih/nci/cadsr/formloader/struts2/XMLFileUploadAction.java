475357851c85bcc33152222cb90e2fa78d4a7a68
Sula Rajapakse <sulalith@gmail.com>
2013-09-04 14:08:38 -0400

    XML validation code

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/struts2/XMLFileUploadAction.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/struts2/XMLFileUploadAction.java
@@ -3,10 +3,16 @@ package gov.nih.nci.cadsr.formloader.struts2;
 
 import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.interceptor.ServletRequestAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import gov.nih.nci.cadsr.formloader.domain.FormCollection;
+import gov.nih.nci.cadsr.formloader.domain.FormDescriptor;
 import gov.nih.nci.cadsr.formloader.object.FormObj;
 import gov.nih.nci.cadsr.formloader.service.ValidationMockupService;
+import gov.nih.nci.cadsr.formloader.service.common.FormLoaderServiceException;
+import gov.nih.nci.cadsr.formloader.service.impl.XmlValidationServiceImpl;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.UserManagerDAO;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,10 +22,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
  
 public class XMLFileUploadAction extends ActionSupport implements
@@ -36,22 +44,37 @@ ServletRequestAware{
     private HttpServletRequest servletRequest;
     
     private List<FormObj> parsedFormsList = new ArrayList<FormObj>();
-    private Properties configProp = new Properties();
+    private Properties configProp;
+    private File uploadedfile = null;
+    private XmlValidationServiceImpl xmlValidator = null;
+	private List<FormDescriptor> forms;
     
     public void loadProps() {
-    	System.getProperty("user.dir");
-    	System.getProperty("user.home");
+    	InputStream in = null;
+        configProp = new Properties();
     	
     	servletRequest = ServletActionContext.getRequest();
     	String filePath = servletRequest.getSession().getServletContext().getRealPath("/");
     	System.out.println("Server path:" + filePath);
     	
-        InputStream in = this.getClass().getResourceAsStream(filePath+"/resources/config.properties");
         try {
+            File f = new File(filePath+"/WEB-INF/config.properties");
+            in = new FileInputStream( f );
             configProp.load(in);
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(configProp.getProperty("upload.file.path"));
         }
+        catch ( Exception e ) { in = null; }
+               
+        try {
+            if ( in == null ) {
+                // Try loading from classpath
+            	in = this.getClass().getResourceAsStream(filePath+"/WEB-INF/config.properties");
+                // Try loading properties from the file (if found)
+                configProp.load(in);
+                System.out.println(configProp.getProperty("upload.file.path"));
+            }
+        }
+        catch ( Exception e ) { }
     }
     
     public File getFile() {
@@ -114,10 +137,10 @@ ServletRequestAware{
     	
     	try
 	    	{
-	    	    File bfile = new File(configProp.getProperty("upload.file.path") + xmlFile.getName());
+    			uploadedfile = new File(configProp.getProperty("upload.file.path") +"\\" + this.fileName);
 	 
 	    	    inStream = new FileInputStream(xmlFile);
-	    	    outStream = new FileOutputStream(bfile);
+	    	    outStream = new FileOutputStream(uploadedfile);
 	 
 	    	    byte[] buffer = new byte[1024];
 	 
@@ -143,12 +166,33 @@ ServletRequestAware{
 
 	public void setUpload(File xmlFile) {
 		formCollection = new FormCollection();
-		saveUploadedFile(xmlFile);
 		this.file = xmlFile;
 	}
 
+	private void validateXML()
+	{
+		
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext(
+				"/applicationContext-service-test-db.xml");
+		XmlValidationServiceImpl xmlValidator = (XmlValidationServiceImpl)applicationContext.getBean("XmlValidationServiceImpl");
+		
+		FormCollection aColl = new FormCollection();
+		aColl.setXmlPathOnServer(".\\test\\data");
+		aColl.setXmlFileName("3193449_has_valid_values.xml");
+		try {
+			aColl = xmlValidator.validateXml(aColl);
+			forms = aColl.getForms();
+		} catch (FormLoaderServiceException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public void setUploadFileName(String xmlFileName) {
 		this.fileName = xmlFileName;
+		saveUploadedFile(this.file);
+		validateXML();
 	}
 
 	public void setUploadContentType(String xmlFileContentType) {
