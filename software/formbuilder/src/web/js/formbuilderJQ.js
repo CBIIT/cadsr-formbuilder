cd92bc3b643545a4d7767ca30ab98f6f096f4a59
anw <david.an@nih.gov>
2013-09-11 12:23:18 -0400

    Manual Add formbuilder

---
+++ software/formbuilder/src/web/js/formbuilderJQ.js
@@ -0,0 +1,141 @@
+
+var un = $.cookie('FormbuilderUsername');
+var pw;
+var nun = $.cookie('newFormbuilderUsername');
+/***
+$(function () {
+////alert("function()");		
+	$('.marqeeMessages').marquee('pointer').mouseover(function () {
+        $(this).trigger('stop');
+    }).mouseout(function () {
+        $(this).trigger('start');
+    }).mousemove(function (event) {
+        if ($(this).data('drag') == true) {
+            this.scrollLeft = $(this).data('scrollX') + ($(this).data('x') - event.clientX);
+        }
+    }).mousedown(function (event) {
+        $(this).data('drag', true).data('x', event.clientX).data('scrollX', this.scrollLeft);
+    }).mouseup(function () {
+        $(this).data('drag', false);
+    });
+});   
+***/ 
+//-->
+
+
+function setupUser()
+{
+	var myInputun = $("#myInputUserName").val();
+	////alert(myInputun);
+		
+	if( myInputun != "viewer/" )  //logout
+    {
+		$(".viewer").hide();
+		$(".noneViewer").show();
+
+		$("#urViewer").hide();
+		$("#noneViewer").show();
+
+		$("#idLogout").show();
+    }
+	else  //login
+    {
+		$(".noneViewer").hide();
+		$(".viewer").show();
+
+		$("#noneViewer").hide();
+		$("#urViewer").show();
+		
+		$("#idLogin").show();
+		
+	    $("input.viewerDisable").attr("disabled", true);
+    }
+	
+	var myFormAdded = $("#myInputMyFormAdded").val();
+	if( myFormAdded != "Y/" && myInputun != "viewer/" )
+		$(".noneViewerAddFormCart").show();
+	else
+		$(".noneViewerAddFormCart").hide();
+			
+}
+
+function setupLink()
+{
+	var cdeBrowserlink = $("#idCDEBrowser").attr('href');
+	
+	var strAdm="cadsradmin";
+	var n = cdeBrowserlink.search("-");
+	
+	if( n == 18 )
+	{
+		var cadsrAdminLink = "http://" + strAdm + cdeBrowserlink.substring(n);
+		////alert(cadsrAdminLink);
+		$("#idCaDSRAdmin").attr('href', cadsrAdminLink);		
+	}
+}
+
+function displayAdditionalMessage( )
+{
+	var myInputun = $("#myInputUserName").val();		
+	var myInfo = $("#myInputAdditionalMessage").val();		
+	var currentPage = $("#myInputCurrentPage").val();
+	var formHasBeenAdded = $("#myInputMyFormAdded").val();
+
+	var info = "No form is added in the Form Cart.";
+	var p1 = "No form is added in the Form Cart.";
+	var p2 = "No form is added in the Form Cart.";
+	if( currentPage == "Result" )
+	{	
+		var n = myInfo.replace("/","");
+		if( n != "0" )
+		{
+			if( n == "1" )
+			{
+				info = n + " form queued in Form Cart for saving. Please go to Form Cart to save it.";
+
+				p1 = n + " form queued in Form Cart for saving."
+				p2 = "Please go to Form Cart to save it.";
+			}
+			else
+			{
+				info = n + " forms queued in Form Cart for saving. Please go to Form Cart to save them.";
+
+				p1 =  n + " forms queued in Form Cart for saving."
+				p2 = "Please go to Form Cart to save them."
+			}
+			
+			$('#p1Message').show().delay( 200 );
+			$('#p2Message').show().delay( 200 );
+			$('#p1Message').html(p1);
+			$('#p2Message').html(p2);
+		}
+		else
+		{
+			$('#p1Message').hide().delay( 200 );
+			$('#p2Message').hide().delay( 200 );
+		}
+	}
+	else 
+	{
+		if( formHasBeenAdded == "Y/" )
+		{		
+			info = "This form has been queued in Form Cart for saving. When done, please go to Form Cart to save it.";
+			
+			p1 = "This form has been queued in Form Cart for saving."
+			p2 = "When done, please go to Form Cart to save it.";
+
+			$('#p1Message').show().delay( 200 );
+			$('#p2Message').show().delay( 200 );
+			$('#p1Message').html(p1);
+			$('#p2Message').html(p2);
+		}
+		else
+		{
+			$('.marqeeMessages').hide().delay( 200 );
+			
+			$('#p1Message').hide().delay( 200 );
+			$('#p2Message').hide().delay( 200 );			
+		}
+	}		
+}
+
---
+++ software/formbuilder/src/web/jsp/formbuilder/formbuilder.jsp
@@ -0,0 +1,79 @@
+<%@ page contentType="text/html;charset=windows-1252"%>
+<%@page import="gov.nih.nci.ncicb.cadsr.common.util.* " %>
+<%@page import="gov.nih.nci.ncicb.cadsr.common.CaDSRConstants"%>
+<%@page import="gov.nih.nci.ncicb.cadsr.common.resource.Context"%>
+<%@page import="java.util.HashMap"%>
+<%@page import="gov.nih.nci.ncicb.cadsr.contexttree.TreeConstants"%>
+<%@ taglib uri="/WEB-INF/tld/struts-logic.tld" prefix="logic"%>
+<%@ taglib uri="/WEB-INF/tld/struts-html.tld" prefix="html"%>
+<%@ taglib uri="/WEB-INF/tld/struts-bean.tld" prefix="bean"%>
+<%@ taglib uri="/WEB-INF/tld/cdebrowser.tld" prefix="cde"%>
+
+<%
+  String ctepUser = (String)pageContext.getAttribute("accessValue");
+  String treeURL;
+  String formbuilderURL;
+    treeURL = 
+      "/jsp/treeLoader.jsp?"+
+      "&treeParams="+TreeConstants.TREE_TYPE_URL_PARAM +":" + 
+      TreeConstants.FORM_SEARCH_TREE + ";" +
+      TreeConstants.FUNCTION_NAME_URL_PARAM + ":" +
+      TreeConstants.FORM_SEARCH_FUNCTION + ";" +
+      "&treeName=formTree" +
+      "&skin=CDEBrowser1";
+    formbuilderURL = "/jsp/formbuilder/formResults.jsp";
+
+    System.out.println(formbuilderURL + " tree url " + treeURL);
+    
+%>
+
+<%@ page session="true" %>
+
+<HTML>
+<HEAD>
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=windows-1252">
+<META HTTP-EQUIV="Cache-Control" CONTENT="no-cache">
+<TITLE>
+FormBuilder
+</TITLE>
+
+<LINK REL=STYLESHEET TYPE="text/css" HREF="<%=request.getContextPath()%>/css/ui-lightness/jquery-ui-1.10.3.custom.min.css">
+<script src="./js/jquery-1.9.1.js"></SCRIPT>
+<script src="./js/jquery-ui-1.10.3.custom.min.js"></SCRIPT>
+<script src="./js/jquery.cookie.js"></SCRIPT>
+<script src="./js/jquery.marquee.js"></script>
+<script src="./js/formbuilderJQ.js"></script>
+
+<script type="text/javascript">
+
+$(document).ready(function()
+{
+	setupUser();	
+});
+
+</script>	
+
+</HEAD>
+  <jsp:useBean id="requestMap" scope="request" class="java.util.HashMap"/>
+  
+<frameset rows="30,*">
+  <html:frame page="/jsp/common/topHeader.jsp" frameborder="0" scrolling = "no" frameName="tree_header"/>
+  <frameset cols="25%,*">
+    <frameset rows="7%,*">
+       <html:frame page="/jsp/common/tree_hdr.jsp"
+              name="requestMap"
+              frameborder="0"
+              frameName="tree_header"
+              scrolling = "no"/>
+       <html:frame page='<%=treeURL%>'
+              name="requestMap"
+              frameborder="0"
+              frameName="tree"/>              
+    </frameset>
+       <html:frame action="/setMessagesForFrameAction?method=setMessagesFormKeys"
+              name="requestMap"
+              frameborder="0"
+              frameName="body"/>      
+  </frameset>
+</frameset>   
+</HTML>
