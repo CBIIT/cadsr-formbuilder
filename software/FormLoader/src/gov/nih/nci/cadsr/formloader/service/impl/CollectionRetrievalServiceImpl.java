f70c44fb6a580650739c4d053840c9bf80af5729
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2013-10-01 18:05:08 -0400

    Added getAllFormsByUser()

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/impl/CollectionRetrievalServiceImpl.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/service/impl/CollectionRetrievalServiceImpl.java
@@ -1,11 +1,15 @@
 package gov.nih.nci.cadsr.formloader.service.impl;
 
 import gov.nih.nci.cadsr.formloader.domain.FormCollection;
+import gov.nih.nci.cadsr.formloader.domain.FormDescriptor;
 import gov.nih.nci.cadsr.formloader.repository.FormLoaderRepository;
 import gov.nih.nci.cadsr.formloader.service.CollectionRetrievalService;
 import gov.nih.nci.cadsr.formloader.service.common.FormLoaderServiceException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
@@ -33,14 +37,58 @@ public class CollectionRetrievalServiceImpl implements CollectionRetrievalServic
 	
 	@Override
 	public List<FormCollection> getAllCollectionsByUser(String userName) throws FormLoaderServiceException {
-		
 		//Not check user credential. Check on unload
 		
+		if (userName == null || userName.length() == 0) 
+			throw new FormLoaderServiceException(FormLoaderServiceException.ERROR_USER_INVALID, 
+					"User name is null or empty. Unable to get collections previously loader by user");
+		
 		List<FormCollection> colls = repository.getAllLoadedCollectionsByUser(userName);
-		if (colls != null)
-			logger.debug("Retrieved " + colls.size() + " form collections from database created by user [" + userName + "]");
+		if (colls == null || colls.size() == 0) {
+			logger.info("User [" + userName + "] doesn't seem have loaded any colleciton previously.");
+			return new ArrayList<FormCollection>();
+		}
 		
 		return colls;
+	}
+
+	@Override
+	public List<FormDescriptor> getAllFormsByUser(String userName)
+			throws FormLoaderServiceException {
+
+		List<FormCollection> colls = getAllCollectionsByUser(userName);
+		
+		List<FormDescriptor> forms = convertCollectionsToFormList(colls);
+		
+		return forms;
+	}
+	
+	/**
+	 * This is to convert a collection based list to form based, with collections as form's attribute
+	 * 
+	 * @param colls
+	 * @return
+	 */
+	protected List<FormDescriptor> convertCollectionsToFormList(List<FormCollection> colls) {
+		
+		if (colls == null || colls.size() == 0) 
+			return new ArrayList<FormDescriptor>();
+		
+		Map<String, FormDescriptor> formTracker = new HashMap<String, FormDescriptor>();
+		
+		for (FormCollection coll : colls) {
+			List<FormDescriptor> forms = coll.getForms();
+			for (FormDescriptor form : forms) {
+				if (!formTracker.containsKey(form.getFormSeqId())) {
+					form.getBelongToCollections().add(coll);
+					formTracker.put(form.getFormSeqId(), form);
+				} else {
+					formTracker.get(form.getFormSeqId()).getBelongToCollections().add(coll);
+				}
+			}
+		}
+		
+		return new ArrayList<FormDescriptor>(formTracker.values());
 	}
 
 	
