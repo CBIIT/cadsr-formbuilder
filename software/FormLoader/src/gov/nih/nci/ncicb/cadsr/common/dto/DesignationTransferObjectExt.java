d85c68d089bbf06aaa67d978b9df53dfe95d6ba5
shanyang-saicf <shan.yang@nih.gov>
2014-02-19 14:04:00 -0500

    Adding missing files from merge of 4.1 branch -> master

---
+++ software/FormLoader/src/gov/nih/nci/ncicb/cadsr/common/dto/DesignationTransferObjectExt.java
@@ -0,0 +1,15 @@
+package gov.nih.nci.ncicb.cadsr.common.dto;
+
+public class DesignationTransferObjectExt extends DesignationTransferObject {
+	String contextName;
+
+	public String getContextName() {
+		return contextName;
+	}
+
+	public void setContextName(String contextName) {
+		this.contextName = contextName;
+	}
+
+	
+}
