468e3e7c57e9c85eecbdf3c950f7701d6a7d9d4a
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2014-03-04 12:06:49 -0500

    Refactoring FormLoaderRepositoryImpl

--- software/FormLoader/test/java/gov/nih/nci/cadsr/formloader/repository/FormLoaderRepositoryImplTest.java
+++ software/FormLoader/test/java/gov/nih/nci/cadsr/formloader/repository/FormLoaderRepositoryImplTest.java
@@ -20,17 +20,12 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/applicationContext-service-test-db.xml"})
+@ContextConfiguration(locations = {"classpath:/applicationContext.xml"})
 public class FormLoaderRepositoryImplTest {
 	
 	@Autowired
 	FormLoaderRepository repository;
-	@Autowired
-	ContentValidationService contentValidationService;
-	@Autowired
-	XmlValidationService xmlValidator;
-	@Autowired
-	LoadingServiceImpl loadService;
+	
 	
 	
 	FormCollection aColl;
@@ -42,7 +37,6 @@ public class FormLoaderRepositoryImplTest {
 
 	@Before
 	public void setUp() throws Exception {
-		prepareCollectionToLoad();
 	}
 	
 	@Test
@@ -78,62 +72,5 @@ public class FormLoaderRepositoryImplTest {
 		}
 	}
 	
-	protected void prepareCollectionToLoad() {
-		assertNotNull(loadService);
-		
-		//String testfile = "3256357_v1_0_newform.xml";
-		//String testfile = "3256357_v1_0_newform-partial.xml";
-		//String testfile = "3256357_v1_0_multi-protocols-newform.xml";
-		//String filepath = ".\\test\\data";
-		
-		
-		try {
-			aColl = new FormCollection();
-			aColl.setXmlPathOnServer(filepath);
-			aColl.setXmlFileName(testfile);
-			
-			aColl = xmlValidator.validateXml(aColl);
-			List<FormDescriptor> forms = aColl.getForms();
-			assertNotNull(forms);
-			assertTrue(forms.size() == 1);
-			
-			FormDescriptor form = forms.get(0);
-			form.setSelected(true);
-			//String status = StatusFormatter.getStatusInXml(form);
-			//StatusFormatter.writeStatusToXml(status, filepath + "\\LoadServiceTest-xmlVal.xml");
-			
-			assertTrue(forms.get(0).getLoadStatus() == FormDescriptor.STATUS_XML_VALIDATED);
-		
-			//aColl.setForms(forms);
-			aColl.setName("Testing Create New Form");
-			aColl.setCreatedBy("yangs");
-			aColl.setXmlFileName(testfile);
-			aColl.setXmlPathOnServer(filepath);
-			
-			assertNotNull(contentValidationService);
-			form.setSelected(true);
-			aColl = contentValidationService.validateXmlContent(aColl);
-			
-			String status = StatusFormatter.getStatusInXml(form);
-			StatusFormatter.writeStatusToXml(status, filepath + "\\LoadServiceTest-content.xml");
-			
-			assertNotNull(aColl);
-			forms = aColl.getForms();
-			assertTrue(forms.size() == 1);
-			assertTrue(forms.get(0).getLoadStatus() == FormDescriptor.STATUS_CONTENT_VALIDATED);
-			
-			form = forms.get(0);
-			
-			List<QuestionDescriptor> questions = forms.get(0).getModules().get(0).getQuestions();
-			assertTrue(questions.size() == 2);
-			assertTrue(questions.get(1).getPublicId() != null);
-			assertTrue(questions.get(1).getCdePublicId().equals("2341940"));
-
-			status = StatusFormatter.getStatusInXml(form);
-			StatusFormatter.writeStatusToXml(status, filepath + "\\LoadService-before.xml");
-		} catch (FormLoaderServiceException fle) {
-			fail("Got exception: " + fle.getMessage());
-		}
-	}
-
+	
 }
