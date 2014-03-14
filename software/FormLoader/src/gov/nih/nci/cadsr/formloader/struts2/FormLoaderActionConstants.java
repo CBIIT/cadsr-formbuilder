45f3bfd14920b7439580ff662d57edb12d97d540
yangs8 <yangs8@NCI-S077257-L.nci.nih.gov>
2014-03-14 12:54:52 -0400

    New constant file to keep session key strings in one place

---
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/struts2/FormLoaderActionConstants.java
@@ -0,0 +1,9 @@
+package gov.nih.nci.cadsr.formloader.struts2;
+
+public class FormLoaderActionConstants {
+	
+	public static String KEY_USER_NAME = "username";
+	public static String KEY_LOGIN = "login";
+	
+
+}
