6f2dd268cbd4e9a88b74c90f2b943e4ed6a5987a
unknown <tanj3@NCI-01870532-L.nci.nih.gov>
2015-01-29 10:40:20 -0500

    Fixed FL ant local deploy issue on Windows platform

--- software/FormLoader/src/gov/nih/nci/cadsr/formloader/struts2/LoadFormsAction.java
+++ software/FormLoader/src/gov/nih/nci/cadsr/formloader/struts2/LoadFormsAction.java
@@ -42,7 +42,7 @@ public class LoadFormsAction extends ActionSupport implements SessionAware{
     	servletRequest = ServletActionContext.getRequest();
     	
         try {
-        	FormCollection aColl = (FormCollection)servletRequest.getSession().getAttribute("formCollection");
+        	FormCollection aColl = (FormCollection)servletRequest.getSession().getAttribute("formCollection");	//JR367 at this point, the instruction is already "missing"
         	
         	if (selectedFormIndices != null && selectedFormIndices.length > 0) {
         		setSelectForFormsInCollections(aColl, selectedFormIndices);
