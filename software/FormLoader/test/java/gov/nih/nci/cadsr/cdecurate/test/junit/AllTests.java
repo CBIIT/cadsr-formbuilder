f9a06e1db32f6ba9a0ca68a29c968fe6e4316d8c
James Tan <james.tan@nih.gov>
2015-02-24 14:37:45 -0500

    Updated with more tests in the test suite

--- software/FormLoader/test/java/gov/nih/nci/cadsr/cdecurate/test/junit/AllTests.java
+++ software/FormLoader/test/java/gov/nih/nci/cadsr/cdecurate/test/junit/AllTests.java
@@ -1,11 +1,56 @@
 package gov.nih.nci.cadsr.cdecurate.test.junit;
 
+import gov.nih.nci.cadsr.formloader.domain.QuestionDescriptorTest;
+import gov.nih.nci.cadsr.formloader.repository.FormLoaderRepositoryImplTest;
+import gov.nih.nci.cadsr.formloader.repository.LoadServiceRepositoryImplTest;
+import gov.nih.nci.cadsr.formloader.service.common.FormLoaderHelperTest;
+import gov.nih.nci.cadsr.formloader.service.common.StaXParserTest;
+import gov.nih.nci.cadsr.formloader.service.impl.CollectionRetrievalServiceImplTest;
+import gov.nih.nci.cadsr.formloader.service.impl.ContentValidationServiceImplTest;
+import gov.nih.nci.cadsr.formloader.service.impl.LoadingServiceImplTest;
+import gov.nih.nci.cadsr.formloader.service.impl.UnloadingServiceImplTest;
+import gov.nih.nci.cadsr.formloader.service.impl.XmlValidationServiceImplTest;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCAdminComponentDAOV2Test;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCClassificationSchemeDAOV2Test;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCCollectionDAOTest;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCContactCommunicationDAOV2Test;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCDefinitionDAOTest;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCDesignationDAOTest;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCFormV2DAOTest;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCFormValidValueDAOV2Test;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCModuleDAOV2Test;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCProtocolDAOV2Test;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCQuestionDAOV2Test;
+import gov.nih.nci.ncicb.cadsr.common.persistence.dao.jdbc.JDBCValueDomainDAOV2Test;
+
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ 
-	JR417.class
+	JR417.class,
+	JDBCAdminComponentDAOV2Test.class,
+	JDBCClassificationSchemeDAOV2Test.class,
+	JDBCCollectionDAOTest.class,
+	JDBCContactCommunicationDAOV2Test.class,
+	JDBCDefinitionDAOTest.class,
+	JDBCDesignationDAOTest.class,
+	JDBCFormV2DAOTest.class,
+	JDBCFormValidValueDAOV2Test.class,
+	JDBCModuleDAOV2Test.class,
+	JDBCProtocolDAOV2Test.class,
+	JDBCQuestionDAOV2Test.class,
+	JDBCValueDomainDAOV2Test.class,
+	CollectionRetrievalServiceImplTest.class,
+	ContentValidationServiceImplTest.class,
+	LoadingServiceImplTest.class,
+	UnloadingServiceImplTest.class,
+	XmlValidationServiceImplTest.class,
+//	FormLoaderHelperTest.class,
+//	StaXParserTest.class,
+	FormLoaderRepositoryImplTest.class,
+	LoadServiceRepositoryImplTest.class,
+	QuestionDescriptorTest.class
 })
 
 public class AllTests {
