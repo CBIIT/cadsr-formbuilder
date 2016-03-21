6632f412306108115d2a2b298e454beb578beff0
purnimac <purnimac@yahoo.com>
2016-03-21 09:14:51 -0400

    FORMBUILD-393 Null check fix

--- software/formbuilder/src/java/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/FormAction.java
+++ software/formbuilder/src/java/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/FormAction.java
@@ -75,7 +75,7 @@ public class FormAction extends FormBuilderSecureBaseDispatchActionWithCarts {
     {
     	for( String str: s )
     	{
-    		if(str.matches( "^.*(%|<|>).*$" ))
+    		if(str != null && str.matches( "^.*(%|<|>).*$" ))
     		{
     			return false;
     		}
